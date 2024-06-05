dd24ae97cd89b99234ff39bb96cc5d6f4188a059
Roiyki <141772658+Roiyki@users.noreply.github.com>
2024-06-05 18:16:15 +0300

    added a run tests file for my ci

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import pytest
 
 # Add the project directory to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from app.Backend.main import app
 
